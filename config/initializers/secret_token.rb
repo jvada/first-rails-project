7dea0474c9d6a65a1250b935096240d63e2e49ac
Your Full Name <jvada28@gmail.com>
2012-09-12 15:11:06 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseProject1::Application.config.secret_token = '7895618ad5affa7f303dc8e54ab0a1d6e6b967c7d327668b35ff78e7833d20ccb1cd2cf35623f8e59a553f47324685a03857b976f195efbfa3483aa42affce76'
